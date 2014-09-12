a9a4c50dca951097528881e438603ba95b357a34
Matt Horning <matt.horning@sq1.com>
2014-09-11 21:26:30 -0500

    first try at the event rolling and failover messaging

--- api/classes/action-handler.php
+++ api/classes/action-handler.php
@@ -18,20 +18,24 @@
 
     public function handleAction() {
       // A 'ping' that exists for whatever reason we need it for, I guess.
-      if ($this->actionString == "login"){
-        $this->gameData['prompt'] = "Thanks for logging in, you're now connected!";
-        $this->gameData['session'] = "XXX";
-        $this->gameData['choices'] = array();
-        $this->gameData['currentEvent'] = "none";
-      }
+
 
       if($this->actionString === 'test' || $this->actionString === 'ping') {
         $this->gameData['prompt'] = 'This is a test of the Rhizome Broadcasting
         System. It does absolutely nothing.';
         $this->gameData['choices'] = array();
       }
-      
-      if (isset($this->gameData['currentEvent'])){
+      //event related stuff goes here
+      if (isset($this->gameData['currentEvent']) && $this->gameData['currentEvent'] != null){
+        //event roller
+        if($this->gameData['currentEvent'] == "none" && $this->actionString == "1"){
+          if (rand(1,10) <= 3 ){
+            $this->gameData['currentEvent'] = 'loot';
+          }else{
+            $this->gameData['currentEvent'] = 'battle';
+          }
+        }
+        
         if($this->gameData['currentEvent'] === 'loot') {
           $loot = new LootHandler($this->gameData, $this->actionString);
           $this->gameData = $loot->makeLoot();
@@ -42,10 +46,18 @@
           $battle = new BattleHandler($this->gameData, $this->actionString);
           $this->gameData = $battle->battle();
         }
+      }else if ($this->actionString == "login"){
+        $this->gameData['prompt'] = "Thanks for logging in, you're now connected!";
+        $this->gameData['session'] = "XXX";
+        $this->gameData['choices'] = array();
+        //do location stuff here
+        array_push($this->gameData['choices'], "To adventure into the great beyond");
+        $this->gameData['currentEvent'] = "none";
+      }else{
+        //this shouldn't happen, but if it does, it might as well be amusing
+        $this->gameData['prompt'] = "You walk into a very dark caverous structure with one flickering blue light in the far corner.  As you slowly approach, unsure of what to make of the situation, the smell of Cheetos and Mountain Dew strikes your nostrils sharply.  Before you can process what is happening, a horrific creature abruptly turns around and snarls at you &#39;What are you doing here human?!&#39;.  You've found an angry developer and this doesn't bode well for you.  Tough break champ.";
       }
-
-
-
+      
       return $this->gameData;
     }
   }
