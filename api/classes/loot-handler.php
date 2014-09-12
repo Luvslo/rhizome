964bda2502610aec99be3d5e7e8422a00487e1a3
Matt Horning <matt.horning@sq1.com>
2014-09-11 20:10:53 -0500

    added basic loot handler and some COOL words

---
+++ api/classes/loot-handler.php
@@ -0,0 +1,11 @@
+<?php
+  class LootHandler {
+    public function __construct($gameData = array(), $string = '') {
+      $this->gameData     = $gameData;
+    }
+
+    public function makeLoot() {
+      
+    }
+  }
+?>
